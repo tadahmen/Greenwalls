04073a9703c35c015ed9ca055622f52a1864a416
tadahmen <tadahmen@gmail.com>
2016-04-04 22:28:56 +0200

    db seeded with (links to) plant pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 Plant.destroy_all
 Plant.create ([
-  {name: "Rose"},
-  {name: "Thyme"},
-  {name: "Hyacinth"}
+  {name: "Rose", picture: "https://c7.staticflickr.com/4/3017/2618479990_26451c14d9_b.jpg"},
+  {name: "Thyme", picture:"http://healthimpactnews.com/wp-content/uploads/sites/2/2014/05/Thyme-leaves.jpg"},
+  {name: "Hyacinth", picture: "https://upload.wikimedia.org/wikipedia/commons/b/b3/Hyacinth_Macaw.jpg"}
   ])
