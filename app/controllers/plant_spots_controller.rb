a11ffe2e822f5f327c061dcf80dbe018b0e8d718
tadahmen <tadahmen@gmail.com>
2016-04-06 11:27:11 +0200

    changed plantspot controller index, just to try

--- app/controllers/plant_spots_controller.rb
+++ app/controllers/plant_spots_controller.rb
@@ -1,14 +1,22 @@
 class PlantSpotsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  before_action :set_plants_container
+
   def index
     render json: {
       meta: {
-        count: PlantSpot.all.count,
+        count: @plants_container.plant_spots.count,
         page: 0
       },
-      plant_spots: PlantSpot.all.order(:name)
+      plant_spots: @plants_container.plant_spots.order(:x_position)
     }
+  end
+
+  private
+
+  def set_plants_container
+    @plants_container = PlantsContainer.find(params[:plants_container_id])
   end
 
 end
