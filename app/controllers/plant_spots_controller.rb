b8eb3e487f13a5ba6d757170f57648e0a5195c45
tadahmen <tadahmen@gmail.com>
2016-04-11 19:56:24 +0200

    selected plants get saved

--- app/controllers/plant_spots_controller.rb
+++ app/controllers/plant_spots_controller.rb
@@ -13,8 +13,27 @@ class PlantSpotsController < ApplicationController
     }
   end
 
+  def update
+    plant_spot = PlantSpot.find(params[:id])
+
+    if plant_spot.update(plant_spot_params)
+      render json: { plant_spot: plant_spot }
+    else
+      render json: {
+        message: "Could not update plant spot",
+        errors: plant_spot.errors,
+      }, status: :unprocessible_entity
+    end
+  end
+
+  private
+
   def set_plants_container
     @plants_container = PlantsContainer.find(params[:plants_container_id])
+  end
+
+  def plant_spot_params
+    params.require(:plant_spot).permit(:plants_container_id, :plant_id)
   end
 
 end
