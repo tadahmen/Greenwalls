a4b47dcfc7eabcb3d1e830443fd3ca77aed9eabe
tadahmen <tadahmen@gmail.com>
2016-04-14 19:09:20 +0200

    seeded db with some new plants

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,6 @@ Plant.create ([
   {name: "Thyme", picture:"http://healthimpactnews.com/wp-content/uploads/sites/2/2014/05/Thyme-leaves.jpg"},
   {name: "Hyacinth", picture: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR6BtX2baeseES00IcH74kzqdUHh1qNaRuP4M_tKfQqZZcvnx1Cmw"},
   {name: "Bird!", picture: "https://upload.wikimedia.org/wikipedia/commons/b/b3/Hyacinth_Macaw.jpg"},
-  {name: "A fish", picture: "http://www.precut.nl/afbeeldingen-G/Goudvissen/goudvis4.png"}, 
   {name: "Tuinkabouter", picture: "https://bestel.nl/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/Outdoor/zombie_tuinkabouter/Zombie_Tuinkabouter500.jpg"}
 ])
 
