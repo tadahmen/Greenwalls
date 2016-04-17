c3512a297a955bb6155295fef9ccd039a2a284d2
tadahmen <tadahmen@gmail.com>
2016-04-17 19:21:24 +0200

    some change in the cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,5 +1,5 @@
 class Greenwalls::Application
-  config.middleware.insert_before 0,  "Rack::Cors", debug: true, logger: (-> { Rails.logger }) do #debug: Rails.env.development?,
+  config.middleware.insert_before 0,  "Rack::Cors", debug: true, logger: (-> { Rails.logger }) do
     allow do
       origins 'localhost:3000', '127.0.0.1:3000',
         #'....bitballoon.com',
